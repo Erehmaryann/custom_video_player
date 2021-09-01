8e6f38c6cec212b843dd6a80dc3115fc0432cb43
Ereh Maryann <edwardmaryann09@gmail.com>
2021-09-01 16:29:15 -0700

    first commit of this project

---
+++ script.js
@@ -0,0 +1,12 @@
+// Select all the needed HTML elements
+// and store them in variables
+
+// Play & Pause ----------------------------------- //
+
+// Progress Bar ---------------------------------- //
+
+// Volume Controls --------------------------- //
+
+// Change Playback Speed -------------------- //
+
+// Fullscreen ------------------------------- //
