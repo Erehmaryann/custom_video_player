1b976a68adfd5eeb83186ebc1640598ade077612
Ereh Maryann <edwardmaryann09@gmail.com>
2021-09-03 11:09:17 -0700

    selected the 2nd html element needed and stored it in a var

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Select all the needed HTML elements
 // and store them in variables
 const videoEl = document.querySelector(".video");
+const progressRange = document.querySelector(".progress-range");
 
 // Play & Pause ----------------------------------- //
 
